a8d8482303fa4e59c0ff29ba69dcf9743e57d8c2
Emen <skrubsquadproductions@gmail.com>
2017-12-18 17:40:41 -0500

    Added a last name and first name button

--- mySubString.js
+++ mySubString.js
@@ -1,12 +1,11 @@
-function testFunction () {
-  var text = document.getElementById('text-box');
 
-  //For searching index of specified character
+
+  //For providing index of specified character or number
   String.prototype.myIndexOf = function(a) {
 
     if(typeof(a) == 'string'){
       var x = 0;
-      while(x<this.length){
+      while(x <= this.length){
         if(this[x] == a){
           var index = x;
         }
@@ -34,13 +33,31 @@ function testFunction () {
 
   }
 
-  //test function
-  firstName = function(name) {
+
+
+
+
+//test functions
+function firstName() {
+  var text = document.getElementById('text-box');
+
+  first = function(name) {
     var firstname = name.mySubString(0, " ");
     return (firstname);
   }
 
-  console.log('First Name: ' + firstName(text.value));
-  alert('Here is your first name: ' + firstName(text.value));
+  console.log('First Name: ' + first(text.value));
+  alert('Here is your first name: ' + first(text.value));
+}
 
+function lastName() {
+  var text = document.getElementById('text-box');
+
+  last = function(name) {
+    var lastname = name.mySubString(" ", name.length);
+    return (lastname);
+  }
+
+  console.log('Last Name: ' + last(text.value));
+  alert('Here is your last name: ' + last(text.value));
 }
